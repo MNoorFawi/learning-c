fa46a358a5402686d156f94e047bda3cddc65729
Muhammad N. Fawi <m.noor.fawi@gmail.com>
2020-08-21 21:59:03 +0400

    Update double_pointer.c

--- dummy code/double_pointer.c
+++ dummy code/double_pointer.c
@@ -16,29 +16,34 @@ int main()
     clock_t t;
     t = clock();
 
-    struct dummy *a, *b;
+    struct dummy *a, *b, *c;
     a = (struct dummy*)malloc(sizeof(struct dummy));
+	c = (struct dummy*)malloc(sizeof(struct dummy));
     a->x = 3;
     b = a;
+	c->x = 7;
 
     puts("");
     printf("a->x is %i\n", a->x);
     printf("b->x is %i\n", b->x);
+	printf("c->x is %i\n", c->x);
 
     puts("\nNow Changing x only in b to 4...");
 
     b->x = 4;
     printf("b->x is now %i\n", b->x);
     printf("a->x is now %i\n", a->x);
+	printf("c->x is now %i\n", c->x);
 
     puts("\n\tdouble pointer to manipulate pointers inside functions ...");
     puts("\nChanging a->x inside a function to 13");
     change_x(&a);
     printf("b->x is now %i\n", b->x);
     printf("a->x is now %i\n", a->x);
+	printf("c->x is now %i\n", c->x);
     printf("Address of a is %p while address of b is %p\n", &a, &b);
-    printf("Address of x in a is %p while address of x in b is %p\n", &a->x, &b->x);
-    puts("\n\tTwo different pointers with different addresses pointing to the same value");
+    printf("Address of x in a is %p and address of x in b is %p \nwhile address of x in c is %p\n", &a->x, &b->x, &c->x);
+    puts("\n\tTwo different pointers with different addresses pointing to the same value (a & b) \nwhile c is having its own x value");
 
     puts("\nNow time ...");
     t = clock() - t;
